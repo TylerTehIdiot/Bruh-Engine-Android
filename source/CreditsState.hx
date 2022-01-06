b315d7feeb47c43759306cab0b7678308bcbde94
TylerTehNub <93739827+TylerTehNub@users.noreply.github.com>
2022-01-05 19:32:42 -0500

    Added My self lmao just gotta add the icon

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -46,8 +46,9 @@ class CreditsState extends MusicBeatState
 		add(grpOptions);
 
 		var pisspoop = [ //Name - Icon name - Description - Link - BG Color
-			['Psych Engine Android Port'],
-			['M.A. Jigsaw',		    'majigsaw',		    'Main Coder of The Port',	 'https://www.youtube.com/channel/UC2Sk7vtPzOvbVzdVTWrribQ',	'0xFFC30085'],
+			['Bruh Engine Android Port'],
+			['M.A. Jigsaw',		    'majigsaw',		    'Psych Engine Android',	 'https://www.youtube.com/channel/UC2Sk7vtPzOvbVzdVTWrribQ',	'0xFFC30085'],
+			['TylerTehNub',		    'tylol',		    'Main Coder of Bruh Engine',	 'https://twitter.com/Ty_1991',	'0xFFFFFFFF'],
 			['Psych Engine Team'],
 			['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',					'https://twitter.com/Shadow_Mario_',	'0xFFFFDD33'],
 			['RiverOaken',			'riveroaken',		'Main Artist/Animator of Psych Engine',				'https://twitter.com/river_oaken',		'0xFFC30085'],
